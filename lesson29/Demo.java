0049673af57b60dbc0415f74f158de3e70bf8d1d
teodory <teodorparapanov@gmail.com>
2016-08-23 17:27:00 +0300

    Homework complete

--- lesson29/Demo.java
+++ lesson29/Demo.java
@@ -1,7 +1,6 @@
 package lesson29;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -13,17 +12,28 @@ import java.util.concurrent.Future;
  */
 public class Demo {
 
+    private static HashMap<String, Integer> words = new HashMap<String, Integer>();
+
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-        for (int i = 0; i < 5; i++) {
+//       1. Play with threads
+//        for (int i = 0; i < 5; i++) {
+//            long start = System.currentTimeMillis();
+//            System.out.println(readCommas(10));
+//            long stop = System.currentTimeMillis();
+//
+//            System.out.println(stop - start);
+//        }
 
-            long start = System.currentTimeMillis();
-            System.out.println(readCommas(10));
-            long stop = System.currentTimeMillis();
+//        2. Count words
+//        countWords();
 
-            System.out.println(stop - start);
-        }
+//        3. print words sorted by length
+//        printWordsByLength();
 
+//        4. make files by the length of the words and add them in
+
+        makeFilesWithWordsByLength();
     }
 
     private static int readCommas(int threads) throws InterruptedException, ExecutionException {
@@ -58,11 +68,7 @@ public class Demo {
 
     private static List<String> getTextParts(int threads) throws FileNotFoundException {
 
-        File book = new File("C:\\Users\\Parapanov\\Desktop\\Lev_Tolstoj_-_Vojna_i_mir_-_Pyrvi_i_vtori_tom-4403-b\\Voina_i_mir.txt");
-//        if(!book.exists()){
-//            throw new FileNotFoundException("Book not found!");
-//            return null;
-//        }
+        File book = new File("Voina_i_mir.txt");
         StringBuilder bookText = new StringBuilder();
         Scanner scanner = new Scanner(book);
 
@@ -83,5 +89,101 @@ public class Demo {
         }
 
         return parts;
+    }
+
+    private static void countWords() {
+        try {
+            extractWordsFromText();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        words.entrySet()
+				.stream()
+				.sorted((v1, v2) -> v1.getValue().compareTo(v2.getValue()))
+				.forEach(
+						en -> System.out.println(en.getKey() + " - "
+								+ en.getValue()));
+		;
+    }
+
+    private static void printWordsByLength() {
+        try {
+            extractWordsFromText();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        words.keySet().stream().sorted((v1,v2)->{
+            if(v2.length()<v1.length()){
+                return 1;
+            }
+            else if(v2.length()>v1.length()){
+                return -1;
+            }
+            return v2.compareTo(v1);
+
+        }).forEach(word->System.out.println(word));
+    }
+
+    private static void extractWordsFromText() throws FileNotFoundException {
+        File book = new File("Voina_i_mir.txt");
+        InputStream stream = new FileInputStream(book);
+        Scanner scanner = new Scanner(stream);
+        while (scanner.hasNext()) {
+            String[] wordWithSymbols = scanner.next().toLowerCase().trim()
+                    .split("[-_.,\\s\\d/?!:„;()×—*=“«…\uFEFF№]+");
+            if(wordWithSymbols.length==0){
+                continue;
+            }
+            String word = wordWithSymbols[0];
+            if(word.length() == 0){
+                continue;
+            }
+
+            if (words.containsKey(word)) {
+                int count = words.get(word);
+                count++;
+                words.put(word, count);
+            } else {
+                words.put(word, 1);
+            }
+        }
+    }
+
+    private static void makeFilesWithWordsByLength(){
+        try {
+            extractWordsFromText();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        for (String word : words.keySet()) {
+
+            int currentLength = word.length();
+            String fileName = "words with length" + currentLength+".txt";
+            File currentFile = new File(fileName);
+
+            if(!currentFile.exists()){
+
+                try {
+                    currentFile.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            try{
+                    FileWriter fileWriter = new FileWriter(currentFile, true);
+                    fileWriter.write(word + ", ");
+                    fileWriter.flush();
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
